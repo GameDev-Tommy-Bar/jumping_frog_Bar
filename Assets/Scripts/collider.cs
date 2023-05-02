1a7e13a0b424dded47bf319499c5b52e2db42089
bar-schtalman <barschtalman@gmail.com>
2023-05-02 18:31:32 +0300

    Update collider.cs

--- Assets/Scripts/collider.cs
+++ Assets/Scripts/collider.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class collider : MonoBehaviour
 {
+    public GameObject GameObjectToDisable1;
+    public GameObject GameObjectToDisable2;
+    public GameObject GameObjectToDisable3;
+    public GameObject winner;
+
+
+
     [SerializeField] string triggeringTag;
     // Start is called before the first frame update
     void Start()
@@ -20,11 +28,24 @@ public class collider : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "top_grass"  && enabled){
             Debug.Log("yessssssssss");
+            Disable();
+            Enable();
         }
         if(other.tag == "car" && enabled){
             Destroy(this.gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
            Debug.Log("game over!");
         }
         
+    }
+    public void Disable()
+    {
+        Destroy(GameObjectToDisable1);
+        Destroy(GameObjectToDisable2);
+        Destroy(GameObjectToDisable3);
+    }
+    public void Enable(){
+        winner.GetComponent<SpriteRenderer>().enabled = true;
+
     }
 }
